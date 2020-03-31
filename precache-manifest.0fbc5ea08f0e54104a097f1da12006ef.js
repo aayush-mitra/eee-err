aac96335853862e3a2f2084fa013763a25f9f9da
EpicodeCFK <epicodecfk@gmail.com>
2020-03-30 20:13:25 -0500

    Version 1.0.1

---
+++ precache-manifest.0fbc5ea08f0e54104a097f1da12006ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af44ccd61065f2a98490bccd722fa97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d2417d788e735a6a87",
+    "url": "/static/css/main.f26ecb44.chunk.css"
+  },
+  {
+    "revision": "c36f623cead38ddf4d81",
+    "url": "/static/js/2.b2abb4d6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b2abb4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d2417d788e735a6a87",
+    "url": "/static/js/main.b152a740.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "ab51426f6ba1af829d4a5cd17145914a",
+    "url": "/static/media/top-bg.ab51426f.jpg"
+  }
+]);
